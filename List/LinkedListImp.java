19b98ac4441162dc64fdb809e50cee94b1391af1
ganavijayaram <jayaram.g@northeastern.edu>
2023-09-18 14:18:38 -0400

    LinkedList using the LinkedList implementing the List Interface

---
+++ List/LinkedListImp.java
@@ -0,0 +1,19 @@
+package List;
+
+import java.util.LinkedList;
+
+public class LinkedListImp {
+    public static void main(){
+        LinkedList<String> linkedList = new LinkedList<>();
+        linkedList.add("one");
+        linkedList.add("two");
+        linkedList.get(0);
+        linkedList.set(0, "newone");
+        linkedList.remove();
+        for(String list: linkedList){
+
+        }
+
+
+    }
+}
