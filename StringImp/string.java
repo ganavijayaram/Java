847f33220529a468e650f65831051acce9fdbdab
ganavijayaram <jayaram.g@northeastern.edu>
2023-09-18 09:41:22 -0400

    String and its methods

--- StringImp/string.java
+++ StringImp/string.java
@@ -1,2 +1,21 @@
-package StringImp;public class string {
+package StringImp;
+
+public class string {
+    public static void main() {
+        String str = "ABC";
+        str.toLowerCase();
+        str.toUpperCase();
+        str.charAt(0);
+        str.compareTo(str);
+        str.contains("a");
+        str.endsWith("b");
+        str.indexOf(str);
+        str.isBlank();
+        str.isEmpty();
+        str.lastIndexOf(str);
+        str.length();
+        str.replace("a", "b");
+        str.startsWith(str);
+    }
+
 }
