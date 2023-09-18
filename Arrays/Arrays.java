4b7081691eab4f07a02d928a127bd2a8d60c932b
ganavijayaram <jayaram.g@northeastern.edu>
2023-09-18 12:21:54 -0400

    Arrays

--- Arrays/Arrays.java
+++ Arrays/Arrays.java
@@ -1,2 +1,27 @@
-package Arrays;public class Arrays {
+package Arrays;
+
+import java.util.OptionalInt;
+import java.util.*;
+
+public class Arrays {
+    public static void main(String args[]){
+        int num  =4;
+        int[] arr = new int[10];
+        Integer[] arrInt = new Integer[10];
+        arr[0] = 10;
+        arr[1] = 20;
+
+        int[] cloneArray = arr.clone();
+        int len = arr.length;
+        OptionalInt max = java.util.Arrays.stream(arr).max();
+        //System.out.println(max.getAsInt());
+        // Sorting array
+        java.util.Arrays.sort(arr);
+        // Binary Search
+        java.util.Arrays.binarySearch(arr, num);
+        // Equals
+        java.util.Arrays.equals(arr, arr);
+        java.util.Arrays.deepEquals(arrInt, arrInt);
+
+    }
 }
