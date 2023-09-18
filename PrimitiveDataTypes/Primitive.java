df26842d2ce59b0a719ccddc369f7345bd8d7f37
ganavijayaram <jayaram.g@northeastern.edu>
2023-09-18 09:08:23 -0400

    Primitive Datatypes

---
+++ PrimitiveDataTypes/Primitive.java
@@ -0,0 +1,21 @@
+package PrimitiveDataTypes;
+
+public class Primitive {
+    public static void main() {
+        int x;
+        byte b;
+        short s;
+        long l;
+        float f;
+        double d;
+        boolean bool;
+        char c;
+        int maxInt = Integer.MAX_VALUE;
+        int minInt = Integer.MIN_VALUE;
+        double maxDouble = Double.MAX_VALUE;
+        double minDouble = Double.MIN_VALUE;
+
+    }
+}
+
+
---
+++ StringImp/string.java
@@ -0,0 +1,2 @@
+package StringImp;public class string {
+}
